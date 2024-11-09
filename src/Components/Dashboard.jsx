580f0d70137c07060c82d1f33e7a9ac1123576dc
kowserahmed121 <kowser.dev@gmail.com>
2024-11-10 00:28:26 +0600

    add item in dashboard and show local storage item

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import ReuseText from "./ReuseText";
+import { allLikeCoffee } from "../Utilis";
+import Card from "./Card";
+
 const Dashboard = () => {
+  const [coffee, setCoffee] = useState([]);
+  useEffect(() => {
+    const likeCoffee = allLikeCoffee();
+    setCoffee(likeCoffee);
+  }, []);
   return (
     <div>
-      <h2>Dashboard</h2>
+      <ReuseText
+        title="Welcome to our Coffee Cafe Dashboard"
+        paragraph="my react router coffee project. this is dynamic coffee shop cafe page."
+      ></ReuseText>
+      <div className="grid grid-cols-3 gap-6 mb-8">
+        {coffee.map((card) => (
+          <Card card={card} key={card.id}></Card>
+        ))}
+      </div>
     </div>
   );
 };
